6ef8a187270e225141a8d1fe019580754eacd920
BrianSammit <briansammit.cruz@gmail.com>
2023-02-28 23:37:16 -0500

    Add EstimatedShippingTime ov

---
+++ src/main/java/com/brian/eCommerce/domain/values/EstimatedShippingTime.java
@@ -0,0 +1,22 @@
+package com.brian.eCommerce.domain.values;
+
+import com.brian.eCommerce.generic.ValueObject;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class EstimatedShippingTime implements ValueObject<Date> {
+    
+    private final Date value;
+
+    public EstimatedShippingTime(Date value) {
+        this.value = Objects.requireNonNull(value);
+        if(this.value == null){
+            throw new IllegalArgumentException("The time is not valid");
+        }
+    }
+    @Override
+    public Date value() {
+        return value;
+    }
+}
