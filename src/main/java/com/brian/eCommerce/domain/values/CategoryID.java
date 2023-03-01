a928e95aaa1da74bbee5d751ef47e2db3d92ba1d
BrianSammit <briansammit.cruz@gmail.com>
2023-02-28 23:45:09 -0500

    Add CategoryID identity

---
+++ src/main/java/com/brian/eCommerce/domain/values/CategoryID.java
@@ -0,0 +1,13 @@
+package com.brian.eCommerce.domain.values;
+
+import com.brian.eCommerce.generic.Identity;
+
+public class CategoryID extends Identity {
+    public CategoryID(){}
+    private CategoryID(String id) {
+        super(id);
+    }
+    public static CategoryID of(String id) {
+        return new CategoryID(id);
+    }
+}
