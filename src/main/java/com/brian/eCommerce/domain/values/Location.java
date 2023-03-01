396561ad30ef60131928d5d354e91d8d1a1a96b1
BrianSammit <briansammit.cruz@gmail.com>
2023-02-28 23:42:13 -0500

    Add Location ov

---
+++ src/main/java/com/brian/eCommerce/domain/values/Location.java
@@ -0,0 +1,21 @@
+package com.brian.eCommerce.domain.values;
+
+import com.brian.eCommerce.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Location implements ValueObject<String> {
+    private final String value;
+
+    public Location(String value) {
+        this.value = Objects.requireNonNull(value);
+        if (this.value.isEmpty()) {
+            throw new IllegalArgumentException("The location is not valid");
+        }
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+}
