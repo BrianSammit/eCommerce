58f14354a17f280aead1224a78f1384b8c9e6f82
BrianSammit <briansammit.cruz@gmail.com>
2023-03-02 15:39:00 -0500

    Add contact details entity

--- src/main/java/com/brian/eCommerce/domain/costumerManagment/ContactDetails.java
+++ src/main/java/com/brian/eCommerce/domain/costumerManagment/ContactDetails.java
@@ -1,4 +1,50 @@
 package com.brian.eCommerce.domain.costumerManagment;
 
-public class ContactDetails {
+import com.brian.eCommerce.domain.values.*;
+import com.brian.eCommerce.generic.Entity;
+
+public class ContactDetails extends Entity<ContactDetailsID> {
+
+    private Country country;
+    private City city;
+    private Address address;
+    private AddressDetails addressDetails;
+    private Email email;
+    private PhoneNum phoneNum;
+
+    public ContactDetails(ContactDetailsID id, Country country, City city,
+                          Address address, AddressDetails addressDetails,
+                          Email email, PhoneNum phoneNum) {
+        super(id);
+        this.country = country;
+        this.city = city;
+        this.address = address;
+        this.addressDetails = addressDetails;
+        this.email = email;
+        this.phoneNum = phoneNum;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public AddressDetails getAddressDetails() {
+        return addressDetails;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public PhoneNum getPhoneNum() {
+        return phoneNum;
+    }
 }
