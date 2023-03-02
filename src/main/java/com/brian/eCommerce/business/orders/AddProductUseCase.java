461f4f835989e8e2a7a2eca4659eae21352b94b7
BrianSammit <briansammit.cruz@gmail.com>
2023-03-02 01:16:06 -0500

    Add product use case

---
+++ src/main/java/com/brian/eCommerce/business/orders/AddProductUseCase.java
@@ -0,0 +1,29 @@
+package com.brian.eCommerce.business.orders;
+
+import com.brian.eCommerce.business.commons.EventRepository;
+import com.brian.eCommerce.business.commons.UseCaseForCommand;
+import com.brian.eCommerce.domain.orders.Order;
+import com.brian.eCommerce.domain.orders.command.AddProductCommand;
+import com.brian.eCommerce.domain.values.*;
+import com.brian.eCommerce.generic.DomainEvent;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AddProductUseCase implements UseCaseForCommand<AddProductCommand> {
+    private  final EventRepository eventRepository;
+
+    public AddProductUseCase(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public List<DomainEvent> apply(AddProductCommand command) {
+        List<DomainEvent> orderEvent = eventRepository.findByAggregatedRootId(command.getProductID());
+        Order order = Order.from(OrderID.of(command.getOrderID()), orderEvent);
+        order.addProduct(ProductID.of(command.getProductID()), new Name(command.getName()), new Price(command.getPrice()),
+                new Description(command.getDescription()), new Location(command.getLocation()),
+                new Quantity(command.getQuantity()));
+        return order.getUncommittedChanges().stream().map(eventRepository::saveEvent).collect(Collectors.toList());
+    }
+}
