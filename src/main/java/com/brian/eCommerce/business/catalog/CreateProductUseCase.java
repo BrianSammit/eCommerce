e5d28472a7686a6f2d43740a4e14dabdf1443463
BrianSammit <briansammit.cruz@gmail.com>
2023-03-02 00:18:50 -0500

    Add CreateProductUseCase

---
+++ src/main/java/com/brian/eCommerce/business/catalog/CreateProductUseCase.java
@@ -0,0 +1,30 @@
+package com.brian.eCommerce.business.catalog;
+
+import com.brian.eCommerce.business.commons.EventRepository;
+import com.brian.eCommerce.business.commons.UseCaseForCommand;
+import com.brian.eCommerce.domain.catalog.Product;
+import com.brian.eCommerce.domain.catalog.command.CreateProductCommand;
+import com.brian.eCommerce.domain.values.*;
+import com.brian.eCommerce.generic.DomainEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CreateProductUseCase implements UseCaseForCommand<CreateProductCommand> {
+
+    private final EventRepository eventRepository;
+
+    public CreateProductUseCase(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public List<DomainEvent> apply(CreateProductCommand command) {
+        Product product = new Product(ProductID.of(command.getProductID()),new Name(command.getName()),
+                new Price(command.getPrice()), new InStock(command.getInStock()), new Description(command.getDescription()),
+                new Publisher(command.getPublisher()), new Location(command.getLocation()));
+        return product.getUncommittedChanges().stream().map(eventRepository::saveEvent).collect(Collectors.toList());
+    }
+}
