af263fac44667e40ccb16e57e034aabfd545a5cb
BrianSammit <briansammit.cruz@gmail.com>
2023-03-01 22:30:29 -0500

    Add OrderCreated event

--- src/main/java/com/brian/eCommerce/domain/orders/events/OrderCreated.java
+++ src/main/java/com/brian/eCommerce/domain/orders/events/OrderCreated.java
@@ -6,17 +6,35 @@ import com.brian.eCommerce.domain.values.ShippingAddress;
 import com.brian.eCommerce.domain.values.Status;
 import com.brian.eCommerce.generic.DomainEvent;
 
+import java.util.Date;
+
 public class OrderCreated extends DomainEvent {
-    protected String date;
+    protected Date date;
     protected Boolean status;
     protected String costumerID;
     protected String shippingAddress;
 
-    public OrderCreated(String type, String date, Boolean status, String costumerID, String shippingAddress) {
+    public OrderCreated(Date date, Boolean status, String costumerID, String shippingAddress) {
         super("eCommerce.orderCreated");
         this.date = date;
         this.status = status;
         this.costumerID = costumerID;
         this.shippingAddress = shippingAddress;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public String getCostumerID() {
+        return costumerID;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
     }
 }
