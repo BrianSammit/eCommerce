ebf4a808a1524b7af65b7c18b65082d8a67dbb40
BrianSammit <briansammit.cruz@gmail.com>
2023-03-02 00:34:13 -0500

    Add CreateOrderUseCase

---
+++ src/main/java/com/brian/eCommerce/business/orders/CreateOrderUseCase.java
@@ -0,0 +1,28 @@
+package com.brian.eCommerce.business.orders;
+
+import com.brian.eCommerce.business.commons.EventRepository;
+import com.brian.eCommerce.business.commons.UseCaseForCommand;
+import com.brian.eCommerce.domain.orders.Order;
+import com.brian.eCommerce.domain.orders.command.CreateOrderCommand;
+import com.brian.eCommerce.domain.values.*;
+import com.brian.eCommerce.generic.DomainEvent;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CreateOrderUseCase implements UseCaseForCommand<CreateOrderCommand> {
+
+    private final EventRepository eventRepository;
+
+    public CreateOrderUseCase(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public List<DomainEvent> apply(CreateOrderCommand command) {
+        Order order = new Order(OrderID.of(command.getOrderID()), new OrderDate(command.getDate()),
+                new Status(command.getStatus()), CostumerID.of(command.getOrderID()),
+                new ShippingAddress(command.getShippingAddress()));
+        return order.getUncommittedChanges().stream().map(eventRepository::saveEvent).collect(Collectors.toList());
+    }
+}
