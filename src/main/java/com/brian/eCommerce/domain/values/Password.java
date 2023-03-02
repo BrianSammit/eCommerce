a61a347507767093d7f8bf6553315fea2b01bca3
BrianSammit <briansammit.cruz@gmail.com>
2023-03-02 15:03:16 -0500

    Add password value object

---
+++ src/main/java/com/brian/eCommerce/domain/values/Password.java
@@ -0,0 +1,21 @@
+package com.brian.eCommerce.domain.values;
+
+import com.brian.eCommerce.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Password implements ValueObject<String> {
+
+    private final String value;
+
+    public Password(String value) {
+        this.value = Objects.requireNonNull(value);
+        if(this.value.isEmpty()){
+            throw new IllegalArgumentException("The status is not valid");
+        }
+    }
+    @Override
+    public String value() {
+        return value;
+    }
+}
