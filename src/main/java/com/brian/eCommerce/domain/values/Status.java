4f1c590617cae3c8ac806a3bbcecbbc9370b0cfb
BrianSammit <briansammit.cruz@gmail.com>
2023-03-01 20:42:56 -0500

    Add status value object

---
+++ src/main/java/com/brian/eCommerce/domain/values/Status.java
@@ -0,0 +1,21 @@
+package com.brian.eCommerce.domain.values;
+
+import com.brian.eCommerce.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Status implements ValueObject<Boolean> {
+
+    private final Boolean value;
+
+    public Status(Boolean value) {
+        this.value = Objects.requireNonNull(value);
+        if(this.value == null){
+            throw new IllegalArgumentException("The status is not valid");
+        }
+    }
+    @Override
+    public Boolean value() {
+        return false;
+    }
+}
