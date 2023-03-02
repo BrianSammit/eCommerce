6129a18e098cca0a6be96c6c63e557a86fd9c07e
BrianSammit <briansammit.cruz@gmail.com>
2023-03-01 20:14:19 -0500

    Add methods to product aggregate root

--- src/main/java/com/brian/eCommerce/domain/catalog/Product.java
+++ src/main/java/com/brian/eCommerce/domain/catalog/Product.java
@@ -1,17 +1,34 @@
 package com.brian.eCommerce.domain.catalog;
 
-import com.brian.eCommerce.domain.catalog.Category;
-import com.brian.eCommerce.domain.catalog.values.ProductID;
-import com.brian.eCommerce.generic.Entity;
+import com.brian.eCommerce.domain.catalog.events.ProductCreated;
+import com.brian.eCommerce.domain.values.*;
+import com.brian.eCommerce.generic.AggregateRoot;
+import com.brian.eCommerce.generic.DomainEvent;
 
 import java.util.List;
 
-public class Product extends Entity<ProductID> {
-
-    private List<Category> categories;
-
-    public Product(ProductID id, List<Category> categories) {
+public class Product extends AggregateRoot<ProductID> {
+    protected Name name;
+    protected Price price;
+    protected InStock inStock;
+    protected Description description;
+    protected Publisher publisher;
+    protected Location location;
+    public Product(ProductID id, String name, Float price, Boolean inStock, String description,
+                   String publisher, String location) {
         super(id);
-        this.categories = categories;
+        subscribe(new ProductEventChange(this));
+        appendChange(new ProductCreated(name, price, inStock, description, publisher, location));
+    }
+
+    public Product(ProductID id) {
+        super(id);
+        subscribe(new ProductEventChange(this));
+    }
+
+    public static Product myProduct(ProductID id, List<DomainEvent> events){
+        Product product = new Product(id);
+        events.forEach(product::applyEvent);
+        return product;
     }
 }
