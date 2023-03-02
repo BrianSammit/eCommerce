d5a353db5a544925b5fea5320b98c97b4721593b
BrianSammit <briansammit.cruz@gmail.com>
2023-03-01 20:12:26 -0500

    Add product created event

--- src/main/java/com/brian/eCommerce/domain/catalog/events/ProductCreated.java
+++ src/main/java/com/brian/eCommerce/domain/catalog/events/ProductCreated.java
@@ -8,22 +8,16 @@ public class ProductCreated extends DomainEvent {
     private final String name;
     private final Float price;
     private final Boolean inStock;
-    private final String image;
     private final String description;
-    private final Date estimateTime;
-    private final String shippingType;
     private final String publisher;
     private final String location;
 
-    public ProductCreated(String name, Float price, Boolean inStock, String image, String description,
-                          Date estimateTime, String shippingType, String publisher, String location,) {
+    public ProductCreated(String name, Float price, Boolean inStock, String description,
+                          String publisher, String location) {
         super("eCommerce.productCreated");
         this.price = price;
         this.inStock = inStock;
-        this.image = image;
         this.description = description;
-        this.estimateTime = estimateTime;
-        this.shippingType = shippingType;
         this.publisher = publisher;
         this.location = location;
         this.name = name;
@@ -41,20 +35,8 @@ public class ProductCreated extends DomainEvent {
         return inStock;
     }
 
-    public String getImage() {
-        return image;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public Date getEstimateTime() {
-        return estimateTime;
-    }
-
-    public String getShippingType() {
-        return shippingType;
     }
 
     public String getPublisher() {
